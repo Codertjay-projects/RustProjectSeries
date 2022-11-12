801b9549c38b656b04f886f2cfcea612dc91881c
Codertjay <codertjay@gmail.com>
2022-11-12 12:26:43 +0100

    t

--- read_json_yt/src/main.rs
+++ read_json_yt/src/main.rs
@@ -1,4 +1,17 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
+struct Paragraph {
+    name: String,
+}
+
+
+#[derive(Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>,
+}
 
 fn main() {
     println!("Hello, world!");
