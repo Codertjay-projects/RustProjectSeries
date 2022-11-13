54e603d4d81383c3fb78ee42856b05624d66dda3
Codertjay <codertjay@gmail.com>
2022-11-13 05:09:40 +0100

    ad api call

--- async-rust-yt/src/main.rs
+++ async-rust-yt/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+
+error_chain! {
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("http://httpbin.org/get").await?;
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+
+    let body = res.text().await?;
+    println!("Body:\n{}", body);
+    Ok(())
 }
